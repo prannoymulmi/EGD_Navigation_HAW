843b2ab208eff664e537091d43f9906a6b8d0021
prannoy <prannoy.mulmi@gmail.com>
2017-10-22 21:29:56 +0200

    Added APIConstants

---
+++ app/src/main/java/navigation/egd/haw/egd_navigation_cj2/constants/APIConstants.java
@@ -0,0 +1,15 @@
+package navigation.egd.haw.egd_navigation_cj2.constants;
+
+/**
+ * @author prannoy
+ * @desciption The constants related with the APIs
+ * Created by prann on 10/20/2017.
+ */
+
+public class APIConstants {
+    public APIConstants(){}
+
+    //TODO: replace with Real API KEYS
+    public static String GOOGLE_DIRECTIONS_API_KEY = "";
+    public static String GOOGLE_DIRECTIONS_API_BASE_URL = "https://maps.googleapis.com/maps/api/directions/json";
+}
