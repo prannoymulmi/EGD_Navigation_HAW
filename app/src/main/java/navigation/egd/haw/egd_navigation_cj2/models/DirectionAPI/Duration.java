47dbee0120451172d1600182a8bef23c5d6d0a64
prannoy <prannoy.mulmi@gmail.com>
2017-10-22 21:28:09 +0200

    Added Models and gradle dependeinces for Direction API

---
+++ app/src/main/java/navigation/egd/haw/egd_navigation_cj2/models/DirectionAPI/Duration.java
@@ -0,0 +1,32 @@
+
+package navigation.egd.haw.egd_navigation_cj2.models.DirectionAPI;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Duration {
+
+    @SerializedName("text")
+    @Expose
+    private String text;
+    @SerializedName("value")
+    @Expose
+    private Integer value;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+}
