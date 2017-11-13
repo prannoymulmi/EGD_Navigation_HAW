1d43b8ff05cd731c01a2794ccdf015bd1214d610
prannoy <prannoy.mulmi@gmail.com>
2017-11-13 10:47:10 +0100

    Added Comments in the Startup Util

--- app/src/main/java/navigation/egd/haw/egd_navigation_cj2/utils/Startup.java
+++ app/src/main/java/navigation/egd/haw/egd_navigation_cj2/utils/Startup.java
@@ -7,6 +7,13 @@ import navigation.egd.haw.egd_navigation_cj2.models.XML.DaggerModuleProviders;
 
 /**
  * This is a singleton class which reads the config files from the xml used to inject dependencies dynamically
+ *
+ * Note: This is a Singleton class and it does not make sense to use this if there is more than one Context contaning classes because
+ * It may be the other class which is gonna use the context will use the incorrect Context.
+ *
+ * Possible solution:
+ * If many Activities or classes containing Context is there the context of the class should be passed to the non activity class as a
+ * dependency.
  * Created by prann on 11/12/2017.
  */
 
@@ -15,7 +22,10 @@ public class Startup {
     private Context context;
 
 
-    //Initialization of the singleton
+    /**
+     * Initialization of the singleton where the context of the activity which can be used throughout the non activity class
+     * @param context
+     */
     public void init(Context context) {
         this.context = context;
         XMLParserUtil parser = new XMLParserUtil();
